4f15576f5d1dd4a25880a6309431cc2f24527395
hollyxj <hollyjordan014@gmail.com>
2024-12-10 16:31:21 -0500

    Update formatting and question numbers

--- create_df.py
+++ create_df.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def clean_df(df):
     # Drop empty columns
     df = df.dropna(axis=1, how='all')
@@ -10,7 +9,8 @@ def clean_df(df):
     # Drop empty rows
     df = df.dropna(axis=0, how='all')
 
-
+    # Drop columns not used in analysis
+    # Includes explicitly dropping empty columns
     columns_to_drop = ['StartDate', 'EndDate', 'Status', 'IPAddress', 'Progress',
                 'Duration (in seconds)', 'Finished', 'RecordedDate', 'ResponseId',
                 'RecipientLastName', 'RecipientFirstName', 'RecipientEmail',
@@ -23,29 +23,30 @@ def clean_df(df):
 
     df = df.drop(columns=columns_to_drop)
 
+    # Give columns relevant names based on the question
     column_names_clean = [
-        # C = Choice
         # TEXT = Written Response from user
-        # R = Rank
-        'Age',
-        'Gender',
-        'Education', 'Education_TEXT',
-        'ScreenTime',
-        'Confidence',
-        'Terms&Conditions', 'ReviewPermissions', 'Cookies',
-        'FamiliarDP',
-        'DefineDP_TEXT',
-        'PressureCreateAccount',
-        'PressureCreateAccountFeel_TEXT',
-        'DeleteAccount',
-        'DeleteAccountExperience_TEXT',
-        'DeceptivePlatformsAndServices_TEXT',
-        'DataRemovalServiceUse',
-        'DataRemovalServiceMotivation_TEXT',
-        'WhichDataRemovalService',
-        'WhichDataRemovalService_TEXT',
-        'CCPA',
-        'UsedLaws'
+        # Q1 is consent question
+        'Age', # Q2
+        'Gender', # Q3
+        'Education', 'Education_TEXT', #Q4
+        'ScreenTime', #Q5
+        'Confidence', #Q6
+        'Terms&Conditions', #Q7
+        'ReviewPermissions', #Q8
+        'Cookies', #Q9
+        'FamiliarDP',#Q10
+        'DefineDP_TEXT', # Q10.1. (Optional)
+        'PressureCreateAccount', # Q11
+        'PressureCreateAccountFeel_TEXT', # Q11.1. (Optional)
+        'DeleteAccount', # Q12
+        'DeleteAccountExperience_TEXT', # Q12.1. (Optional)
+        'DeceptivePlatformsAndServices_TEXT', # Q13
+        'DataRemovalServiceUse', # Q14
+        'DataRemovalServiceMotivation_TEXT', # Q14.1. (Optional)
+        'WhichDataRemovalService',  'WhichDataRemovalService_TEXT', # Q14.2. (Optional)
+        'CCPA', # Q15
+        'UsedLaws' # Q15.1
     ]
 
     # Create a dictionary mapping old column names to new column names
@@ -56,7 +57,6 @@ def clean_df(df):
 
     # Fill NaN values with 0 in all columns
     df.fillna(value=0, inplace=True)
-
 
     return df
 
@@ -69,6 +69,7 @@ def main():
     print(df.info)
 
     # Save the cleaned DataFrame as a pickle file
+    # To avoid the need to create the dataframe every time the analysis is run
     df.to_pickle('survey_12-8.pkl')
     exit()
 
